6c15f5c3eb5cac0ab0eab96c2bcb6b6cb5ab00cb
Wang Zichong <wangzichong@deepin.org>
2023-08-23 17:42:10 +0800

    feat: check X-Deepin-Vendor to see if we need to use GenericName
    检查 desktop 文件的 Vendor，若为 deepin 时，优先使用 GenericName 字段
    
    Log:

--- src/gioutils/appinfomonitor.cpp
+++ src/gioutils/appinfomonitor.cpp
@@ -45,15 +45,22 @@ QList<QHash<QString, QString> > AppInfoMonitor::allAppInfosShouldBeShown()
             if (g_app_info_should_show(app_info)) {
 
                 GIcon * icon = g_app_info_get_icon(app_info);
-                gchar * iconCStr = icon ? g_icon_to_string(icon) : NULL;
+                g_autofree gchar * iconCStr = icon ? g_icon_to_string(icon) : NULL;
                 QString iconStr(iconCStr ? iconCStr : "");
-                if (iconCStr) g_free(iconCStr);
+
+                g_autofree char * xDeepinVendor = g_desktop_app_info_get_string((GDesktopAppInfo*)app_info, "X-Deepin-Vendor");
+                const QString genericName(g_desktop_app_info_get_generic_name((GDesktopAppInfo*)app_info));
+                QString displayName(g_app_info_get_display_name(app_info));
+
+                if (xDeepinVendor && (strcmp(xDeepinVendor, "deepin") == 0) && !genericName.isEmpty()) {
+                    displayName = genericName;
+                }
 
                 const char * const * keywords = g_desktop_app_info_get_keywords((GDesktopAppInfo*)app_info);
                 QStringList keywordsList(fromGStrV(keywords));
 
                 result->append({
-                    {"name", QString(g_app_info_get_display_name(app_info))},
+                    {"name", displayName},
                     {"id", QString(g_app_info_get_id(app_info))},
                     {"filepath", QString(g_desktop_app_info_get_filename((GDesktopAppInfo*)app_info))},
                     {"categories", QString(g_desktop_app_info_get_categories((GDesktopAppInfo*)app_info))},
