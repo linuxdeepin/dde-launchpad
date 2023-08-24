459cba1fc1f016f0aacf9d9d69ef878dff1eda5f
Hosted Weblate <hosted@weblate.org>
2023-08-24 10:46:01 +0200

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 85.2% (29 of 34 strings)
    
    Translated using Weblate (Ukrainian)
    
    Currently translated at 85.2% (29 of 34 strings)
    
    Translated using Weblate (Italian)
    
    Currently translated at 70.5% (24 of 34 strings)
    
    Translated using Weblate (Hungarian)
    
    Currently translated at 85.2% (29 of 34 strings)
    
    i18n: Added translation using Weblate (Portuguese (Brazil))
    
    i18n: Added translation using Weblate (Ukrainian)
    
    i18n: Added translation using Weblate (Italian)
    
    Translated using Weblate (Hungarian)
    
    Currently translated at 2.9% (1 of 34 strings)
    
    i18n: Added translation using Weblate (Hungarian)
    
    Co-authored-by: Béla Bajza <bajzabela@gmail.com>
    Co-authored-by: Gary Wang <wzc782970009@gmail.com>
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: deepin community <support@deepin.org>
    Translate-URL: https://hosted.weblate.org/projects/deepin/dde-launchpad/hu/
    Translate-URL: https://hosted.weblate.org/projects/deepin/dde-launchpad/it/
    Translate-URL: https://hosted.weblate.org/projects/deepin/dde-launchpad/pt_BR/
    Translate-URL: https://hosted.weblate.org/projects/deepin/dde-launchpad/uk/
    Translation: deepin/Launcher (dde-launchpad)

---
+++ translations/dde-launchpad_it.ts
@@ -0,0 +1,190 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it">
+<context>
+    <name>AppItemMenu</name>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="26"/>
+        <source>Open</source>
+        <translation>Apri</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="35"/>
+        <source>Pin to Top</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="40"/>
+        <source>Remove from favorites</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="40"/>
+        <source>Add to favorites</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="50"/>
+        <source>Send to desktop</source>
+        <translation>Invia al desktop</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="50"/>
+        <source>Remove from desktop</source>
+        <translation>Rimuovi dal desktop</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="52"/>
+        <source>Remove from dock</source>
+        <translation>Rimuovi dalla dock</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="52"/>
+        <source>Send to dock</source>
+        <translation>Invia alla dock</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="62"/>
+        <source>Add to startup</source>
+        <translation>Aggiungi all&apos;avvio automatico</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="62"/>
+        <source>Remove from startup</source>
+        <translation>Rimuovi dall&apos;avvio automatico</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="63"/>
+        <source>Use a proxy</source>
+        <translation>Usa un proxy</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="66"/>
+        <source>Uninstall</source>
+        <translation>Disinstalla</translation>
+    </message>
+</context>
+<context>
+    <name>AppListView</name>
+    <message>
+        <location filename="../qml/AppListView.qml" line="34"/>
+        <source>Internet</source>
+        <translation>Internet</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppListView.qml" line="36"/>
+        <source>Chat</source>
+        <translation>Chat</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppListView.qml" line="38"/>
+        <source>Music</source>
+        <translation>Musica</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppListView.qml" line="40"/>
+        <source>Video</source>
+        <translation>Video</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppListView.qml" line="42"/>
+        <source>Graphics</source>
+        <translation>Grafica</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppListView.qml" line="44"/>
+        <source>Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/AppListView.qml" line="46"/>
+        <source>Office</source>
+        <translation>Ufficio</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppListView.qml" line="48"/>
+        <source>Reading</source>
+        <translation>Lettura</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppListView.qml" line="50"/>
+        <source>Development</source>
+        <translation>Sviluppo</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppListView.qml" line="52"/>
+        <source>System</source>
+        <translation>Sistema</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppListView.qml" line="54"/>
+        <source>Others</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FullscreenFrame</name>
+    <message>
+        <location filename="../qml/FullscreenFrame.qml" line="64"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LauncherController</name>
+    <message>
+        <location filename="../launchercontroller.cpp" line="19"/>
+        <source>Show launcher (hidden by default)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../launchercontroller.cpp" line="20"/>
+        <source>Toggle launcher visibility</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WindowedFrame</name>
+    <message>
+        <location filename="../qml/WindowedFrame.qml" line="38"/>
+        <source>Categories</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/WindowedFrame.qml" line="112"/>
+        <source>Power</source>
+        <translation>Alimentazione</translation>
+    </message>
+    <message>
+        <location filename="../qml/WindowedFrame.qml" line="125"/>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <location filename="../qml/WindowedFrame.qml" line="149"/>
+        <source>Search</source>
+        <translation>Cerca</translation>
+    </message>
+    <message>
+        <location filename="../qml/WindowedFrame.qml" line="167"/>
+        <source>My Favorites</source>
+        <translation>I miei Preferiti</translation>
+    </message>
+    <message>
+        <location filename="../qml/WindowedFrame.qml" line="198"/>
+        <source>Add your favorite apps here</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/WindowedFrame.qml" line="208"/>
+        <source>All Apps</source>
+        <translation>Tutte le app</translation>
+    </message>
+    <message>
+        <location filename="../qml/FullscreenFrame.qml" line="167"/>
+        <location filename="../qml/WindowedFrame.qml" line="232"/>
+        <source>No search results</source>
+        <translation>Nessun risultato disponibile</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/dde-launchpad_pt_BR.ts
@@ -0,0 +1,190 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>AppItemMenu</name>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="26"/>
+        <source>Open</source>
+        <translation>Abrir</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="35"/>
+        <source>Pin to Top</source>
+        <translation>Fixar no Topo</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="40"/>
+        <source>Remove from favorites</source>
+        <translation>Remover dos Favoritos</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="40"/>
+        <source>Add to favorites</source>
+        <translation>Adicionar aos Favoritos</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="50"/>
+        <source>Send to desktop</source>
+        <translation>Enviar para a área de trabalho</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="50"/>
+        <source>Remove from desktop</source>
+        <translation>Remover da Área de Trabalho</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="52"/>
+        <source>Remove from dock</source>
+        <translation>Remover da Dock</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="52"/>
+        <source>Send to dock</source>
+        <translation>Enviar para a dock</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="62"/>
+        <source>Add to startup</source>
+        <translation>Adicionar à inicialização</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="62"/>
+        <source>Remove from startup</source>
+        <translation>Remover da inicialização</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="63"/>
+        <source>Use a proxy</source>
+        <translation>Usar Proxy</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItemMenu.qml" line="66"/>
+        <source>Uninstall</source>
+        <translation>Desinstalar</translation>
+    </message>
+</context>
+<context>
+    <name>AppListView</name>
+    <message>
+        <location filename="../qml/AppListView.qml" line="34"/>
+        <source>Internet</source>
+        <translation>Internet</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppListView.qml" line="36"/>
+        <source>Chat</source>
+        <translation>Bate-Papo</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppListView.qml" line="38"/>
+        <source>Music</source>
+        <translation>Player de Música</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppListView.qml" line="40"/>
+        <source>Video</source>
+        <translation>Player de Vídeo</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppListView.qml" line="42"/>
+        <source>Graphics</source>
+        <translation>Imagem</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppListView.qml" line="44"/>
+        <source>Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/AppListView.qml" line="46"/>
+        <source>Office</source>
+        <translation>Escritório</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppListView.qml" line="48"/>
+        <source>Reading</source>
+        <translation>Leitura</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppListView.qml" line="50"/>
+        <source>Development</source>
+        <translation>Desenvolvimento</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppListView.qml" line="52"/>
+        <source>System</source>
+        <translation>Sistema</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppListView.qml" line="54"/>
+        <source>Others</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FullscreenFrame</name>
+    <message>
+        <location filename="../qml/FullscreenFrame.qml" line="64"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LauncherController</name>
+    <message>
+        <location filename="../launchercontroller.cpp" line="19"/>
+        <source>Show launcher (hidden by default)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../launchercontroller.cpp" line="20"/>
+        <source>Toggle launcher visibility</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WindowedFrame</name>
+    <message>
+        <location filename="../qml/WindowedFrame.qml" line="38"/>
+        <source>Categories</source>
+        <translation>Categorias</translation>
+    </message>
+    <message>
+        <location filename="../qml/WindowedFrame.qml" line="112"/>
+        <source>Power</source>
+        <translation>Energia</translation>
+    </message>
+    <message>
+        <location filename="../qml/WindowedFrame.qml" line="125"/>
+        <source>Settings</source>
+        <translation>Configurações</translation>
+    </message>
+    <message>
+        <location filename="../qml/WindowedFrame.qml" line="149"/>
+        <source>Search</source>
+        <translation>Pesquisar</translation>
+    </message>
+    <message>
+        <location filename="../qml/WindowedFrame.qml" line="167"/>
+        <source>My Favorites</source>
+        <translation>Favoritos</translation>
+    </message>
+    <message>
+        <location filename="../qml/WindowedFrame.qml" line="198"/>
+        <source>Add your favorite apps here</source>
+        <translation>Adicione seus aplicativos favoritos aqui</translation>
+    </message>
+    <message>
+        <location filename="../qml/WindowedFrame.qml" line="208"/>
+        <source>All Apps</source>
+        <translation>Todos os Aplicativos</translation>
+    </message>
+    <message>
+        <location filename="../qml/FullscreenFrame.qml" line="167"/>
+        <location filename="../qml/WindowedFrame.qml" line="232"/>
+        <source>No search results</source>
+        <translation>Nenhum resultado</translation>
+    </message>
+</context>
+</TS>
